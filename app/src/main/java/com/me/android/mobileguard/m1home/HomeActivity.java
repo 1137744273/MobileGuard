2cc7770031370244159f307ce2c67d251f133f36
Rysj0 <1161297560@qq.com>
2017-09-24 20:34:41 +0800

    啦啦啦

---
+++ app/src/main/java/com/me/android/mobileguard/m1home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.me.android.mobileguard.m1home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.me.android.mobileguard.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
