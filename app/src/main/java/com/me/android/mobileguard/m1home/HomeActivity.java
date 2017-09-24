108e06b7840ff73f1764cd8907138bc8ce4691af
Rysj0 <1161297560@qq.com>
2017-09-24 21:44:46 +0800

    第一章

--- app/src/main/java/com/me/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/com/me/android/mobileguard/m1home/HomeActivity.java
@@ -4,12 +4,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.me.android.mobileguard.R;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.me.android.mobileguard.m1home.adapter.HomeAdapter;
 
 public class HomeActivity extends AppCompatActivity {
+    private GridView gv_home;
+    private long mExitTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        getSupportActionBar().hide();//去除标题栏
+        gv_home = (GridView) findViewById(R.id.gv_home);
+        gv_home.setAdapter(new HomeAdapter(HomeActivity.this));
+        gv_home.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if ((System.currentTimeMillis() - mExitTime) < 2000) {
+                System.exit(0);
+            } else {
+                mExitTime = System.currentTimeMillis();
+                Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+            }
+            return true;//拦截回调函数的处理  阻止系统对回退键的默认处理
+
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
