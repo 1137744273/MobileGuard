f558e895d59eaf1a05169e9e6a6f6a1e784930fe
dzh <1137744273@qq.com>
2017-12-17 17:03:27 +0800

    完成,修改了

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/AdvancedToolsActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/AdvancedToolsActivity.java
@@ -44,9 +44,21 @@ public class AdvancedToolsActivity extends Activity implements OnClickListener{
             case R.id.imgv_leftbtn:
                 finish();
                 break;
+            case R.id.advanceview_applock:
+                //进入程序锁页面
+                startActivity(AppLockActivity.class);
+                break;
             case R.id.advanceview_numbelongs:
                 //进入归属地查询页面
                 startActivity(NumBelongtoActivity.class);
+                break;
+            case R.id.advanceview_smsbackup:
+                //进入短信备份页面
+                startActivity(SMSBackupActivity.class);
+                break;
+            case R.id.advanceview_smsreducition:
+                //进入短信还原页面
+                startActivity(SMSReducitionActivity.class);
                 break;
         }
     }
